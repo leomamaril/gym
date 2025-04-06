d49eca0630627c8ce953c2f2d06b0405c1fdea2d
La Mamaril <lamamaril11@gmail.com>
2025-04-06 17:58:10 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <Router basename="/gym">
             <Navbar />
             <Routes>
-                <Route path="/" element={<Home />} />
+               
                 <Route path="/home" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/classes" element={<Classes />} />
